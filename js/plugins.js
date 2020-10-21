bb891571192e1e7ca65ae3f9267aa18b471032a7
ahmed samir <asproducer80@gmail.com>
2020-10-21 04:29:30 +0200

    editnew

--- js/plugins.js
+++ js/plugins.js
@@ -1,20 +1,26 @@
 // trigger nice scroll
+
 $(window).on("load", function() {
-    
-    setTimeout(function () {
-        $("html").niceScroll({
 
-            cursorborder: 0,
-            cursorborderradius: '25px',
-            cursorwidth: '7px',
-            cursorcolor: '#dc3545',
-            cursorfixedheight: 120,
-            zindex:'99999999'
+    let minLg = window.matchMedia("(min-width: 992px)");
+
+    if (minLg.matches) {
+
+        setTimeout(function () {
+            $("body").niceScroll({
+
+                cursorborder: 0,
+                cursorborderradius: '25px',
+                cursorwidth: '7px',
+                cursorcolor: '#fb414e',
+                cursorfixedheight: 120,
+                zindex:'99999999'
 
 
-        });
-    }, 500);
-});    
+            });
+        }, 500);
+    }
+}); 
 
 //trigger mixer
 
