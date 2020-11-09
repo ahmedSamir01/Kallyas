2a424f9b729bb596c2aaa63f9efd1aef32a0d754
ahmed samir <asproducer80@gmail.com>
2020-11-09 17:20:17 +0200

    editnav

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function () {
     
     "use strict";
     
-    
+    new WOW({mobile:!1}).init();
     $(window).on("scroll load", _ => {
        
         if ($(this).scrollTop() > 1000) {
@@ -17,6 +17,57 @@ $(function () {
         }
         
     });
+    
+    let portfolio = document.querySelectorAll(".portfolio img");
+    
+    portfolio.forEach(pic => {
+        
+         pic.onclick = function () {
+             
+             let over = document.createElement('div');
+             over.className = 'popup-over';
+             document.body.appendChild(over);
+             
+             // create popup
+             let popBox = document.createElement('div');
+             popBox.className = 'popup';
+             
+             let close = document.createElement("span");
+             let x = document.createTextNode("x");
+             
+             close.appendChild(x);
+             close.className = 'close';
+             popBox.appendChild(close);
+             
+
+             // create img
+             let popImg = document.createElement('img');
+             popImg.className = "popup-img";
+             popImg.src = pic.src;
+             
+             popBox.appendChild(popImg);
+             document.body.appendChild(popBox);
+    
+             
+         }
+        
+    });
+    
+    document.addEventListener("click", function(e) {
+         
+        if (e.target.className == "close") {
+            
+            e.target.parentElement.remove();
+            document.querySelector(".popup-over").remove();
+        }
+        if (e.target.className == "popup-over") {
+            
+            document.querySelector(".popup").remove();
+            e.target.remove();
+            
+        }
+        
+    });   
     
     $(".chevron").on("click", function () {
         
@@ -41,12 +92,12 @@ $(function () {
         
     });
 
-    
+    // close when click somewhere
+    $(document).bind("click", function(e) {
+        if ($(".navbar-collapse").hasClass("show") && e.target !== document.querySelector(".show") && e.target !== document.querySelector(".navbar-toggler")) {$(".navbar-toggler").click()}
+    });
     
 });
-
-
-
 
 
 
--- js/plugins.js
+++ js/plugins.js
@@ -2,6 +2,7 @@
 
 $(window).on("load", function() {
 
+    
     let minLg = window.matchMedia("(min-width: 992px)");
 
     if (minLg.matches) {
@@ -9,18 +10,17 @@ $(window).on("load", function() {
         setTimeout(function () {
             $("body").niceScroll({
 
-                cursorborder: 0,
-                cursorborderradius: '25px',
-                cursorwidth: '7px',
-                cursorcolor: '#fb414e',
-                cursorfixedheight: 120,
-                zindex:'99999999'
-
+            cursorborder: 0,
+            cursorborderradius: '25px',
+            cursorwidth: '7px',
+            cursorcolor: '#fb414e',
+            cursorfixedheight: 120,
+            zindex:'99999999'
 
             });
         }, 500);
     }
-}); 
+});  
 
 //trigger mixer
 
