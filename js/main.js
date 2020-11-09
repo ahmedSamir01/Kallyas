b7f0cf5492962a3ffbc1b9382e3d5f3d8d96390b
ahmed samir <asproducer80@gmail.com>
2020-11-09 18:11:49 +0200

    editnav5

--- js/main.js
+++ js/main.js
@@ -80,12 +80,12 @@ $(function () {
     });
     
     
-    $("nav ul li a").on("click", function () {
+    $("nav ul li").on("click", function () {
 
         $(".navbar-collapse").removeClass("show");
         $("body, html").animate({
            
-            scrollTop: $("." + $(this).data('scroll')).offset().top
+            scrollTop: $("." + $(this).children.data('scroll')).offset().top
             
         }, 1000);
 
