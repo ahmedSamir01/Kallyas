ff1e517e0040d5a1d323797e2db463837ff5999a
ahmed samir <asproducer80@gmail.com>
2020-11-11 02:39:20 +0200

    updategeneral4

--- js/main.js
+++ js/main.js
@@ -47,6 +47,58 @@ $(function () {
     });
     
     
+    // handeling popup
+
+    $(".portfolio .overlay").each((index, pic) => {
+        
+         pic.onclick = function () {
+             
+             let over = document.createElement('div');
+             over.className = 'popup-over';
+             document.body.appendChild(over);
+             
+             // create popup
+             let popBox = document.createElement('div');
+             popBox.className = 'popup';
+             
+             let close = document.createElement("span");
+             let x = document.createTextNode("x");
+             
+             close.appendChild(x);
+             close.className = 'close';
+             popBox.appendChild(close);
+             
+
+             // create img
+             let popImg = document.createElement('img');
+
+             popImg.src = pic.previousSibling.src;
+             
+             popBox.appendChild(popImg);
+             document.body.appendChild(popBox);
+             
+         }
+        
+    });
+    
+    document.addEventListener("click", function(e) {
+         
+        if (e.target.className == "close") {
+            
+            e.target.parentElement.remove();
+            $(".popup-over").remove();
+        }
+        if (e.target.className == "popup-over") {
+            
+            $(".popup").remove();
+            e.target.remove();
+            
+        }
+        
+    }); 
+    
+    
+    
     $("nav ul li a").on("click", function () {
 
         $(".navbar-collapse").removeClass("show");
