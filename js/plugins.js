2264ea9e568366e274cd4c316b66a0025cc0a923
ahmed samir <asproducer80@gmail.com>
2021-01-13 16:47:08 +0200

    scroll edit

--- js/plugins.js
+++ js/plugins.js
@@ -1,37 +1,22 @@
 // trigger nice scroll
 
-$(window).on("load", function() {
+$(window).on("load", function () {
+  let minLg = window.matchMedia("(min-width: 992px)");
 
-    
-    let minLg = window.matchMedia("(min-width: 992px)");
-
-    if (minLg.matches) {
-
-        setTimeout(function () {
-            $("body").niceScroll({
-
-            cursorborder: 0,
-            cursorborderradius: '25px',
-            cursorwidth: '7px',
-            cursorcolor: '#fb414e',
-            cursorfixedheight: 120,
-            zindex:'99999999'
-
-            });
-        }, 500);
-    }
-});  
+  if (minLg.matches) {
+    setTimeout(function () {
+      $("body").niceScroll({
+        cursorborder: 0,
+        cursorborderradius: "25px",
+        cursorwidth: "7px",
+        cursorcolor: "#fb414e",
+        cursorfixedheight: 120,
+        zindex: "99999999",
+      });
+    }, 500);
+  }
+});
 
 //trigger mixer
 
 $("#container").mixItUp();
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
